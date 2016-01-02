376d7838070d647ba4d15000a4b99b3ad86b5e86
David CLEMENT <David CLEMENT>
2016-01-02 15:52:37 +0100

    ajout lib sqlite

---
+++ Code_CA/projet/src/main/java/javafx/AppliFx.java
@@ -0,0 +1,22 @@
+package javafx;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppliFx extends Application{
+
+
+
+	public static void main(String[] args) {
+		System.out.println( "Main method inside Thread : " +  Thread.currentThread().getName());
+		launch(args);
+	}
+
+	public void start(Stage primaryStage) throws Exception {
+		 primaryStage.setWidth(1024);
+	        primaryStage.setHeight(968);
+	        primaryStage.setTitle("JavaFX Xebia"); 
+	        primaryStage.show();
+	           
+	}
+}
+
