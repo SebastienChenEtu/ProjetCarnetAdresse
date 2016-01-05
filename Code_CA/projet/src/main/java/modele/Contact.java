6105fec27c243b56dd9ecbd85bf2ddf7ec4561a0
Sébastien CHEN <Sbastien CHEN>
2016-01-05 16:44:06 +0100

    -- Rajout de lombok.jar -- pour l'utiliser, prenez le jar et le mettre au niveau du dossier de l'ide. Si votre ide est ouvert, faite le redémarrer.

--- Code_CA/projet/src/main/java/modele/Contact.java
+++ Code_CA/projet/src/main/java/modele/Contact.java
@@ -3,6 +3,9 @@ import java.sql.Blob;
 import java.util.ArrayList;
 import java.util.Date;
 
+import lombok.Data;
+
+@Data
 public class Contact {
 	// il faut le gÃ©nÃ©rer automatiquement
 	private int idContact;
@@ -16,73 +19,7 @@ public class Contact {
 	private int idGroupe;
 	private Blob photo;
 	private Boolean favoris;
-	public int getIdContact() {
-		return idContact;
-	}
-	public void setIdContact(int idContact) {
-		this.idContact = idContact;
-	}
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	public String getPrenom() {
-		return prenom;
-	}
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-	public Date getDdn() {
-		return ddn;
-	}
-	public void setDdn(Date ddn) {
-		this.ddn = ddn;
-	}
-	public String getAdresse() {
-		return adresse;
-	}
-	public void setAdresse(String adresse) {
-		this.adresse = adresse;
-	}
-	public String getTelephone() {
-		return telephone;
-	}
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
-	}
-	public String getFax() {
-		return fax;
-	}
-	public void setFax(String fax) {
-		this.fax = fax;
-	}
-	public String getMail() {
-		return mail;
-	}
-	public void setMail(String mail) {
-		this.mail = mail;
-	}
-	public Blob getPhoto() {
-		return photo;
-	}
-	public void setPhoto(Blob photo) {
-		this.photo = photo;
-	}
-	public Boolean getFavoris() {
-		return favoris;
-	}
-	public void setFavoris(Boolean favoris) {
-		this.favoris = favoris;
-	}
 
-	public int getIdGroupe() {
-		return idGroupe;
-	}
-	public void setIdGroupe(int idGroupe) {
-		this.idGroupe = idGroupe;
-	}
 	public Contact(int idContact, String nom, String prenom, Date ddn, String adresse, String telephone, String fax,
 			String mail, int idGroupe, Blob photo, Boolean favoris) {
 		super();
--- Code_CA/projet/src/main/java/modele/Groupe.java
+++ Code_CA/projet/src/main/java/modele/Groupe.java
@@ -1,26 +1,17 @@
 package modele;
-import java.util.List;
 
+import lombok.Data;
+
+@Data
 public class Groupe {
-	public int getIdGroupe() {
-		return idGroupe;
-	}
-	public void setIdGroupe(int idGroupe) {
-		this.idGroupe = idGroupe;
-	}
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
 	public Groupe(int idGroupe, String nom) {
 		super();
 		this.idGroupe = idGroupe;
 		this.nom = nom;
 	}
-	public Groupe() {
-
+	
+	public Groupe(){
+		
 	}
 	// il faut le gÃ©nÃ©rer automatiquement
 	private int idGroupe;
